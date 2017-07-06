57d2d1e81f4615f63a9e95381de45120b5db39bb
Francesco Bruni <bruni@planetek.it>
2017-07-06 13:28:23 +0200

    Fixed name when sending data to processor

--- src/main/java/it/planetek/marinecmems/managerod/manager/controllers/models/ProcessingInputData.java
+++ src/main/java/it/planetek/marinecmems/managerod/manager/controllers/models/ProcessingInputData.java
@@ -1,7 +1,6 @@
 package it.planetek.marinecmems.managerod.manager.controllers.models;
 
 import org.hibernate.validator.constraints.NotBlank;
-import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.constraints.NotNull;
 import java.util.List;
@@ -11,19 +10,15 @@ import java.util.List;
  */
 public class ProcessingInputData {
 
+    private final String procType = "custom";
     @NotNull
     private List<String> dates;
-
     @NotBlank
     @NotNull
     private String aoi;
-
     @NotBlank
     @NotNull
-    private String product;
-
-
-    private final String procType = "custom";
+    private String products;
 
     public ProcessingInputData() {
     }
@@ -45,11 +40,11 @@ public class ProcessingInputData {
     }
 
     public String getProduct() {
-        return product;
+        return products;
     }
 
     public void setProduct(String product) {
-        this.product = product;
+        this.products = product;
     }
 
     public String getProcType() {
--- src/test/java/it/planetek/marinecmems/managerod/manager/controllers/ProcessingControllerTest.java
+++ src/test/java/it/planetek/marinecmems/managerod/manager/controllers/ProcessingControllerTest.java
@@ -2,17 +2,12 @@ package it.planetek.marinecmems.managerod.manager.controllers;
 
 import it.planetek.marinecmems.managerod.ManagerodApplication;
 import it.planetek.marinecmems.managerod.mailsender.exceptions.ProcessingInputParamsException;
-import it.planetek.marinecmems.managerod.manager.controllers.models.ProcessingModel;
-import it.planetek.marinecmems.managerod.manager.domains.Processing;
 import it.planetek.marinecmems.managerod.manager.services.ProcessingService;
 import it.planetek.marinecmems.managerod.processor.services.ProcessorService;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,20 +17,13 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.transaction.Transactional;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.initMocks;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -74,7 +62,7 @@ public class ProcessingControllerTest {
                 "    \"processingInputData\":{\n" +
                 "        \"dates\":[\"2017-06-20\", \"2017-06-23\"],\n" +
                 "        \"aoi\":\"\",\n" +
-                "        \"product\":\"1\"\n" +
+                "        \"products\":\"1\"\n" +
                 "    }\n" +
                 "}";
         mockMvc.perform(post("/processings")
@@ -91,7 +79,7 @@ public class ProcessingControllerTest {
                 "    \"processingInputData\":{\n" +
                 "        \"dates\":[\"2017-06-20\", \"2017-06-23\"],\n" +
                 "        \"aoi\":\"\",\n" +
-                "        \"product\":\"1\"\n" +
+                "        \"products\":\"1\"\n" +
                 "    }\n" +
                 "}";
         String aaa = mockMvc.perform(post("/processings")
