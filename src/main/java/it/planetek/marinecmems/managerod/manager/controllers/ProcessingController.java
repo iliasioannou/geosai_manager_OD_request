0c77326c5ab67ebca7b36e1e213e8766262fe13d
Francesco Bruni <bruni@planetek.it>
2017-07-06 13:15:28 +0200

    Send mail when processing request has been enqueued

--- src/main/java/it/planetek/marinecmems/managerod/manager/controllers/ProcessingController.java
+++ src/main/java/it/planetek/marinecmems/managerod/manager/controllers/ProcessingController.java
@@ -1,10 +1,11 @@
 package it.planetek.marinecmems.managerod.manager.controllers;
 
 import it.planetek.marinecmems.managerod.mailsender.exceptions.ProcessingInputParamsException;
+import it.planetek.marinecmems.managerod.mailsender.services.MailService;
 import it.planetek.marinecmems.managerod.manager.controllers.models.ProcessingModel;
 import it.planetek.marinecmems.managerod.manager.domains.Processing;
-import it.planetek.marinecmems.managerod.processor.services.ProcessorService;
 import it.planetek.marinecmems.managerod.manager.services.ProcessingService;
+import it.planetek.marinecmems.managerod.processor.services.ProcessorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.HttpStatus;
@@ -12,8 +13,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.Valid;
 
@@ -32,10 +35,14 @@ public class ProcessingController {
     @Autowired
     private ProcessorService processorService;
 
+    @Autowired
+    private MailService mailService;
+
     @ResponseBody
     @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Processing> startProcessing(@Valid @RequestBody  ProcessingModel processingModel, BindingResult bindingResult) throws ProcessingInputParamsException {
         Processing processing = processingService.createNewProcessing(processingModel);
+        mailService.sendMailEnqueuedRequest(processing);
         processorService.startProcessing(processingModel, processing);
         return new ResponseEntity<>(processing, HttpStatus.CREATED);
     }
